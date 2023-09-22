1f8f9e9f7470cd41404d45e0f7596985013bc772
TuqayW <101931875+TuqayW@users.noreply.github.com>
2023-09-22 18:19:24 +0400

    Main
    Main

---
+++ homework.py
@@ -0,0 +1,17 @@
+print("Tapsiriq #1")
+
+name = str(input("Enter Your Name:"))
+
+surname = str(input("Enter your surname:"))
+
+age = int(input("Enter your age:"))
+
+print(f"Your name is {name} and your age is {age} and your surname is {surname}")
+
+print("Tapsiriq #2")
+
+num1= int(input("Enter first number:"))
+
+num2= int(input("Enter second number:"))
+
+print(f"Number 1 + Number 2={num1+num2} \nNumber 1 - Number 2={num1-num2} \nNumber 2 - Number 1={num2-num1} \nNumber 1 * Number 2={num1*num2} \nNumber 1 / Number 2={num1/num2} \nNumber 2 / Number 1={num2/num1}")
